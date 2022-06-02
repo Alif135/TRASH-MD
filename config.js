91a1b56f806997ea821512d12f539be6a2d19d7c
Alif135 <61240413+Alif135@users.noreply.github.com>
2022-06-03 06:30:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
-global.packname = 'Bot Whatsapp'
-global.author = 'TRASH'
-global.sessionName = 'data'
+global.owner = ['6281315017916']
+global.packname = 'Khalif BOTz'
+global.author = 'Khalif'
+global.sessionName = 'session'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'Khalif'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
